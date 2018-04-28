070f7fec8299e368b33cf2dc07c03525fe1b6783
Tim Meier <tmeier@fh-bielefeld.de>
2018-04-28 12:46:23 +0200

    Ich komme dem Grafikmonster auf die Spur

--- Computergraphik/Abgabe1/Blatt01/main.cpp
+++ Computergraphik/Abgabe1/Blatt01/main.cpp
@@ -44,91 +44,24 @@ struct Object
 	glm::mat4x4 model;
 };
 
-Object triangle;
-Object quad;
+Object circle;
 
 void renderCircle()
 {
-
-}
-
-void renderTriangle()
-{
 	// Create mvp.
-	glm::mat4x4 mvp = projection * view * triangle.model;
-
-	// Bind the shader program and set uniform(s).
-	program.use();
-	program.setUniform("mvp", mvp);
-
-	// Bind vertex array object so we can render the 1 triangle.
-	glBindVertexArray(triangle.vao);
-	glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_SHORT, 0);
-	glBindVertexArray(0);
-}
-
-void renderQuad()
-{
-	// Create mvp.
-	glm::mat4x4 mvp = projection * view * quad.model;
+	glm::mat4x4 mvp = projection * view * circle.model;
 
 	// Bind the shader program and set uniform(s).
 	program.use();
 	program.setUniform("mvp", mvp);
 	
 	// Bind vertex array object so we can render the 2 triangles.
-	glBindVertexArray(quad.vao);
+	glBindVertexArray(circle.vao);
 	glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_SHORT, 0);
 	glBindVertexArray(0);
 }
 
-void initTriangle()
-{
-	// Construct triangle. These vectors can go out of scope after we have send all data to the graphics card.
-	const std::vector<glm::vec3> vertices = { glm::vec3(-1.0f, 1.0f, 0.0f), glm::vec3(1.0f, -1.0f, 0.0f), glm::vec3(1.0f, 1.0f, 0.0f) };
-	const std::vector<glm::vec3> colors = { glm::vec3(1.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f), glm::vec3(0.0f, 0.0f, 1.0f) };
-	const std::vector<GLushort> indices = { 0, 1, 2 };
-
-	GLuint programId = program.getHandle();
-	GLuint pos;
-
-	// Step 0: Create vertex array object.
-	glGenVertexArrays(1, &triangle.vao);
-	glBindVertexArray(triangle.vao);
-
-	// Step 1: Create vertex buffer object for position attribute and bind it to the associated "shader attribute".
-	glGenBuffers(1, &triangle.positionBuffer);
-	glBindBuffer(GL_ARRAY_BUFFER, triangle.positionBuffer);
-	glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(glm::vec3), vertices.data(), GL_STATIC_DRAW);
-
-	// Bind it to position.
-	pos = glGetAttribLocation(programId, "position");
-	glEnableVertexAttribArray(pos);
-	glVertexAttribPointer(pos, 3, GL_FLOAT, GL_FALSE, 0, 0);
-
-	// Step 2: Create vertex buffer object for color attribute and bind it to...
-	glGenBuffers(1, &triangle.colorBuffer);
-	glBindBuffer(GL_ARRAY_BUFFER, triangle.colorBuffer);
-	glBufferData(GL_ARRAY_BUFFER, colors.size() * sizeof(glm::vec3), colors.data(), GL_STATIC_DRAW);
-
-	// Bind it to color.
-	pos = glGetAttribLocation(programId, "color");
-	glEnableVertexAttribArray(pos);
-	glVertexAttribPointer(pos, 3, GL_FLOAT, GL_FALSE, 0, 0);
-
-	// Step 3: Create vertex buffer object for indices. No binding needed here.
-	glGenBuffers(1, &triangle.indexBuffer);
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, triangle.indexBuffer);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(GLushort), indices.data(), GL_STATIC_DRAW);
-
-	// Unbind vertex array object (back to default).
-	glBindVertexArray(0);
-
-	// Modify model matrix.
-	triangle.model = glm::translate(glm::mat4(1.0f), glm::vec3(-1.25f, 0.0f, 0.0f));
-}
-
-void initQuad()
+void initCircle()
 {
 	// Construct triangle. These vectors can go out of scope after we have send all data to the graphics card.
 	const std::vector<glm::vec3> vertices = { { -1.0f, 1.0f, 0.0f }, { -1.0, -1.0, 0.0 }, { 1.0f, -1.0f, 0.0f }, { 1.0f, 1.0f, 0.0f } };
@@ -139,12 +72,12 @@ void initQuad()
 	GLuint pos;
 
 	// Step 0: Create vertex array object.
-	glGenVertexArrays(1, &quad.vao);
-	glBindVertexArray(quad.vao);
+	glGenVertexArrays(1, &circle.vao);
+	glBindVertexArray(circle.vao);
 
 	// Step 1: Create vertex buffer object for position attribute and bind it to the associated "shader attribute".
-	glGenBuffers(1, &quad.positionBuffer);
-	glBindBuffer(GL_ARRAY_BUFFER, quad.positionBuffer);
+	glGenBuffers(1, &circle.positionBuffer);
+	glBindBuffer(GL_ARRAY_BUFFER, circle.positionBuffer);
 	glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(glm::vec3), vertices.data(), GL_STATIC_DRAW);
 
 	// Bind it to position.
@@ -153,8 +86,8 @@ void initQuad()
 	glVertexAttribPointer(pos, 3, GL_FLOAT, GL_FALSE, 0, 0);
 
 	// Step 2: Create vertex buffer object for color attribute and bind it to...
-	glGenBuffers(1, &quad.colorBuffer);
-	glBindBuffer(GL_ARRAY_BUFFER, quad.colorBuffer);
+	glGenBuffers(1, &circle.colorBuffer);
+	glBindBuffer(GL_ARRAY_BUFFER, circle.colorBuffer);
 	glBufferData(GL_ARRAY_BUFFER, colors.size() * sizeof(glm::vec3), colors.data(), GL_STATIC_DRAW);
 
 	// Bind it to color.
@@ -163,15 +96,15 @@ void initQuad()
 	glVertexAttribPointer(pos, 3, GL_FLOAT, GL_FALSE, 0, 0);
 
 	// Step 3: Create vertex buffer object for indices. No binding needed here.
-	glGenBuffers(1, &quad.indexBuffer);
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, quad.indexBuffer);
+	glGenBuffers(1, &circle.indexBuffer);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, circle.indexBuffer);
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(GLushort), indices.data(), GL_STATIC_DRAW);
 
 	// Unbind vertex array object (back to default).
 	glBindVertexArray(0);
 
 	// Modify model matrix.
-	quad.model = glm::translate(glm::mat4(1.0f), glm::vec3(1.25f, 0.0f, 0.0f));
+	circle.model = glm::translate(glm::mat4(1.0f), glm::vec3(1.25f, 0.0f, 0.0f));
 }
 
 /*
@@ -209,8 +142,7 @@ bool init()
 	}
 
 	// Create objects.
-	initTriangle();
-	initQuad();
+	initCircle();
 
 	return true;
 }
@@ -232,8 +164,7 @@ void releaseObject(Object& obj)
 void release()
 {
 	// Shader program will be released upon program termination.
-	releaseObject(triangle);
-	releaseObject(quad);
+	releaseObject(circle);
 }
 
 /*
@@ -243,8 +174,7 @@ void render()
 {
 	glClear(GL_COLOR_BUFFER_BIT);
 
-	renderTriangle();
-	renderQuad();
+	renderCircle();
 }
 
 void glutDisplay ()
