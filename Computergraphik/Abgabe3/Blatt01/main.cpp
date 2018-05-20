6d78279cf63ef09f65622f968c11e049b843ea21
Tim Meier <tmeier@fh-bielefeld.de>
2018-05-20 16:56:11 +0200

    It might sound crazy, but it ain't no lie Baby Bye Bye Bye

--- Computergraphik/Abgabe3/Blatt01/main.cpp
+++ Computergraphik/Abgabe3/Blatt01/main.cpp
@@ -8,7 +8,7 @@
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/matrix_inverse.hpp>
-
+#include "glm/gtx/rotate_vector.hpp"
 #include "GLSLProgram.h"
 #include "GLTools.h"
 
@@ -28,7 +28,43 @@ float zNear = 0.1f;
 float zFar  = 100.0f;
 float zoomy = 4.0f;
 
+float xDreh = 1.0f;
+float yDreh = 1.0f;
+float zDreh = 1.0f;
 
+std::vector<glm::vec3> vertices = {
+	//front 0,1,2,3
+{ -1.0,  1.0,  1.0 },//0
+{ 1.0,  1.0,  1.0 },//1
+{ 1.0, -1.0,  1.0 },//2
+{ -1.0, -1.0,  1.0 },//3
+					 //left 0,3,4,6
+{ -1.0,  1.0,  1.0 },//4
+{ -1.0, -1.0,  1.0 },//5
+{ -1.0,  1.0, -1.0 },//6
+{ -1.0, -1.0, -1.0 },//7
+					//back 4,5,6,7
+{ -1.0,  1.0, -1.0 },//8
+{ 1.0,  1.0, -1.0 },//9
+{ 1.0, -1.0, -1.0 },//10
+{ -1.0, -1.0, -1.0 },//11
+					 //right 1,2,5,7
+{ 1.0,  1.0,  1.0 },//12
+{ 1.0, -1.0,  1.0 },//13
+{ 1.0,  1.0, -1.0 },//14
+{ 1.0, -1.0, -1.0 },//15
+					 //bot 2,3,6,7
+{ 1.0, -1.0,  1.0 },//16
+{ -1.0, -1.0,  1.0 },//17
+{ 1.0, -1.0, -1.0 },//18
+{ -1.0, -1.0, -1.0 },//19
+					 //top 0,1,4,5
+{ -1.0,  1.0,  1.0 },//20
+{ 1.0,  1.0,  1.0 },//21
+{ -1.0,  1.0, -1.0 },//22
+{ 1.0,  1.0, -1.0 },//23
+
+};
 
 /*
 Struct to hold data for object rendering.
@@ -48,6 +84,7 @@ struct Object
 };
 
 Object quad;
+Object coordinates;
 
 void renderQuad()
 {
@@ -64,86 +101,105 @@ void renderQuad()
 	glBindVertexArray(0);
 }
 
-void initQuad()
+void renderCoordinates()
+{
+	// Create mvp.
+	glm::mat4x4 mvp = projection * view * coordinates.model;
+
+	// Bind the shader program and set uniform(s).
+	program.use();
+	program.setUniform("mvp", mvp);
+
+	// Bind vertex array object so we can render the 2 triangles.
+	glBindVertexArray(coordinates.vao);
+	glDrawElements(GL_TRIANGLES, 24, GL_UNSIGNED_SHORT, 0);
+	glBindVertexArray(0);
+}
+
+void initCoordinates()
 {
 	// Construct triangle. These vectors can go out of scope after we have send all data to the graphics card.
-	
-	const std::vector<glm::vec3> vertices = {
-		//front 0,1,2,3
-		{ -1.0,  1.0,  1.0 },//0
-		{  1.0,  1.0,  1.0 },//1
-		{  1.0, -1.0,  1.0 },//2
-		{ -1.0, -1.0,  1.0 },//3
+	const std::vector<glm::vec3> vertices = { glm::vec3(-100.0f, 0.0f, 0.0f), glm::vec3(100.0f, 0.01f, 0.0f), glm::vec3(100.0f, 0.0f, 0.0f), 
+		glm::vec3(0.01f, 100.0f, 0.0f), glm::vec3(0.0f, -100.0f, 0.0f), glm::vec3(0.0f, 100.0f, 0.0f),
+		glm::vec3(0.001f, 0.001f, 100.0f), glm::vec3(0.0f, 0.0f, 100.0f), glm::vec3(0.001f, 0.0f, -100.0f) };
+	const std::vector<glm::vec3> colors = { glm::vec3(1.0f, 0.0f, 0.0f), glm::vec3(1.0f, 0.0f, 0.0f), glm::vec3(1.0f, 0.0f, 0.0f), 
+		glm::vec3(0.0f, 1.0f, 0.0f), glm::vec3(0.0f, 1.0f, 1.0f), glm::vec3(0.0f, 1.0f, 1.0f), 
+		glm::vec3(0.0f, 0.0f, 1.0f), glm::vec3(0.0f, 0.0f, 1.0f), glm::vec3(0.0f, 0.0f, 1.0f) };
+	const std::vector<GLushort> indices = { 0, 1, 2, 3, 4, 5, 6, 7, 8};
 
-		//left 0,3,4,6
-		{ -1.0,  1.0,  1.0 },//4
-		{ -1.0, -1.0,  1.0 },//5
-		{ -1.0,  1.0, -1.0 },//6
-		{  1.0, -1.0, -1.0 },//7
+	GLuint programId = program.getHandle();
+	GLuint pos;
 
-		//back 4,5,6,7
-		{ -1.0,  1.0, -1.0 },//8
-		{  1.0,  1.0, -1.0 },//9
-		{  1.0, -1.0, -1.0 },//10
-		{ -1.0, -1.0, -1.0 },//11
+	// Step 0: Create vertex array object.
+	glGenVertexArrays(1, &coordinates.vao);
+	glBindVertexArray(coordinates.vao);
 
-		//right 1,2,5,7
-		{  1.0,  1.0,  1.0 },//12
-		{  1.0, -1.0,  1.0 },//13
-		{  1.0,  1.0, -1.0 },//14
-		{ -1.0, -1.0, -1.0 },//15
-		
-		//bot 2,3,6,7
-		{  1.0, -1.0,  1.0 },//16
-		{ -1.0, -1.0,  1.0 },//17
-		{  1.0, -1.0, -1.0 },//18
-		{ -1.0, -1.0, -1.0 },//19
-	
-		//top 0,1,4,5
-		{ -1.0,  1.0,  1.0 },//20
-		{  1.0,  1.0,  1.0 },//21
-		{ -1.0,  1.0, -1.0 },//22
-		{  1.0,  1.0, -1.0 },//23
+	// Step 1: Create vertex buffer object for position attribute and bind it to the associated "shader attribute".
+	glGenBuffers(1, &coordinates.positionBuffer);
+	glBindBuffer(GL_ARRAY_BUFFER, coordinates.positionBuffer);
+	glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(glm::vec3), vertices.data(), GL_STATIC_DRAW);
 
-	};
+	// Bind it to position.
+	pos = glGetAttribLocation(programId, "position");
+	glEnableVertexAttribArray(pos);
+	glVertexAttribPointer(pos, 3, GL_FLOAT, GL_FALSE, 0, 0);
 
-	/*const std::vector<glm::vec3> vertices = {
-	{-1.0, -1.0,  1.0},
-	{1.0, -1.0,  1.0},
-	{1.0,  1.0,  1.0},
-	{-1.0,  1.0,  1.0},
-	{-1.0, -1.0, -1.0},
-	{1.0, -1.0, -1.0},
-	{1.0,  1.0, -1.0},
-	{-1.0,  1.0, -1.0}
-	};*/
+	// Step 2: Create vertex buffer object for color attribute and bind it to...
+	glGenBuffers(1, &coordinates.colorBuffer);
+	glBindBuffer(GL_ARRAY_BUFFER, coordinates.colorBuffer);
+	glBufferData(GL_ARRAY_BUFFER, colors.size() * sizeof(glm::vec3), colors.data(), GL_STATIC_DRAW);
 
+	// Bind it to color.
+	pos = glGetAttribLocation(programId, "color");
+	glEnableVertexAttribArray(pos);
+	glVertexAttribPointer(pos, 3, GL_FLOAT, GL_FALSE, 0, 0);
+
+	// Step 3: Create vertex buffer object for indices. No binding needed here.
+	glGenBuffers(1, &coordinates.indexBuffer);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, coordinates.indexBuffer);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(GLushort), indices.data(), GL_STATIC_DRAW);
+
+	// Unbind vertex array object (back to default).
+	glBindVertexArray(0);
+
+	// Modify model matrix.
+	coordinates.model = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, 0.0f));
+}
+
+void initQuad()
+{
 	const std::vector<glm::vec3> colors = {
+	//front rot
 	{ 1.0, 0.0, 0.0 },
 	{ 1.0, 0.0, 0.0 },
 	{ 1.0, 0.0, 0.0 },
 	{ 1.0, 0.0, 0.0 },
 	
+	//left gelb
 	{ 1.0, 1.0, 0.0 },
 	{ 1.0, 1.0, 0.0 },
 	{ 1.0, 1.0, 0.0 },
 	{ 1.0, 1.0, 0.0 },
 	
+	//back magenta
 	{ 1.0, 0.0, 1.0 },
 	{ 1.0, 0.0, 1.0 },
 	{ 1.0, 0.0, 1.0 },
 	{ 1.0, 0.0, 1.0 },
 	
+	//right grün
 	{ 0.0, 1.0, 0.0 },
 	{ 0.0, 1.0, 0.0 },
 	{ 0.0, 1.0, 0.0 },
 	{ 0.0, 1.0, 0.0 },
 	
+	//bot blau
 	{ 0.0, 0.0, 1.0 },
 	{ 0.0, 0.0, 1.0 },
 	{ 0.0, 0.0, 1.0 },
 	{ 0.0, 0.0, 1.0 },
 	
+	//top cyan
 	{ 0.0, 1.0, 1.0 },
 	{ 0.0, 1.0, 1.0 },
 	{ 0.0, 1.0, 1.0 },
@@ -151,6 +207,7 @@ void initQuad()
 	};
 	
 	const std::vector<GLushort> indices = {
+
 		//front
 		0, 1, 2,
 		2, 3, 0,
@@ -159,37 +216,17 @@ void initQuad()
 		13, 14, 15,
 		// back
 		8, 9, 10,
-		9, 10, 11,
+		8, 10, 11,
 		// left
 		4, 5, 6,
 		5, 6, 7,
 		// bottom
 		16, 17, 18,
-		16, 18, 19,
+		17, 18, 19,
 		// top
 		20, 21, 22,
 		21, 22, 23
 	};
-
-	/*const std::vector<GLushort> indices = { 
-		0, 1, 2,
-		2, 3, 0,
-		// right
-		1, 5, 6,
-		6, 2, 1,
-		// back
-		7, 6, 5,
-		5, 4, 7,
-		// left
-		4, 0, 3,
-		3, 7, 4,
-		// bottom
-		4, 5, 1,
-		1, 0, 4,
-		// top
-		3, 2, 6,
-		6, 7, 3 
-	};*/
 
 	GLuint programId = program.getHandle();
 	GLuint pos;
@@ -229,6 +266,7 @@ void initQuad()
 	// Modify model matrix.
 	quad.model = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, 0.0f));
 	//glEnable(GL_DEPTH_TEST);
+	glEnable(GL_DEPTH_TEST);
 }
 
 /*
@@ -237,7 +275,7 @@ void initQuad()
 bool init()
 {
 	// OpenGL: Set "background" color and enable depth testing.
-	glClearColor(1.0f, 0.2f, 0.2f, 0.0f);
+	glClearColor(0.2f, 0.2f, 0.2f, 0.0f);
 
 	// Construct view matrix.
 	glm::vec3 eye(0.0f, 0.0f, zoomy);
@@ -267,7 +305,7 @@ bool init()
 
 	// Create objects.
 	initQuad();
-
+	initCoordinates();
 	return true;
 }
 
@@ -289,6 +327,7 @@ void release()
 {
 	// Shader program will be released upon program termination.
 	releaseObject(quad);
+	releaseObject(coordinates);
 }
 
 /*
@@ -296,9 +335,10 @@ void release()
  */
 void render()
 {
-	glClear(GL_COLOR_BUFFER_BIT);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 	renderQuad();
+	renderCoordinates();
 }
 
 void glutDisplay ()
@@ -340,6 +380,27 @@ void zoom() {
 	view = glm::lookAt(eye, center, up);
 }
 
+void rotateX() {
+	float winkel = 0.1f;
+	for (int i = 0; i < vertices.size(); i++) {
+		vertices[i] = glm::rotateX(vertices[i],winkel);
+	}
+}
+
+void rotateY() {
+	float winkel = 0.1f;
+	for (int i = 0; i < vertices.size(); i++) {
+		vertices[i] = glm::rotateY(vertices[i], winkel);
+	}
+}
+
+void rotateZ() {
+	float winkel = 0.1f;
+	for (int i = 0; i < vertices.size(); i++) {
+		vertices[i] = glm::rotateZ(vertices[i], winkel);
+	}
+}
+
 /*
  Callback for char input.
  */
@@ -364,13 +425,22 @@ void glutKeyboard (unsigned char keycode, int x, int y)
 		}
 		break;
 	case 'x':
-		// do something
+		rotateX();
+		releaseObject(quad);
+		glClear(GL_DEPTH_BUFFER_BIT);
+		initQuad();
 		break;
 	case 'y':
-		// do something
+		rotateY();
+		releaseObject(quad);
+		glClear(GL_DEPTH_BUFFER_BIT);
+		initQuad();
 		break;
 	case 'z':
-		// do something
+		rotateZ();
+		releaseObject(quad);
+		glClear(GL_DEPTH_BUFFER_BIT);
+		initQuad();
 		break;
 	}
 	glutPostRedisplay();
@@ -390,7 +460,7 @@ int main(int argc, char** argv)
 
 	glutCreateWindow("Aufgabenblatt 01");
 	glutID = glutGetWindow();
-	  
+
 	// GLEW: Load opengl extensions
 	glewExperimental = GL_TRUE;
 	GLenum result = glewInit();
