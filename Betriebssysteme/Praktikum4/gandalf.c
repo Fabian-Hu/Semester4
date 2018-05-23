16f4978630c98c65c31a1e2b1ca0a2d5d9e3caa9
Tim Meier <meiertim745@gmail.com>
2018-05-23 16:57:31 +0200

    kaputt über kaputt

--- Betriebssysteme/Praktikum4/gandalf.c
+++ Betriebssysteme/Praktikum4/gandalf.c
@@ -5,10 +5,13 @@
 #include <string.h>
 
 int forky();
-
 int programmStarty();
+int sety();
+
+pid_t pid;
 
 int main() {
+	
 	char string[20];
 	char name[50];
 	char pfad[1024];
@@ -25,30 +28,66 @@ int main() {
 			return 1;
 		} else if(strcmp(input,"fork")==0){
 			forky();
+			if(pid==0){
+				return 1;
+			}
+			//system("clear");
 		} else if(strcmp(input,"cd")==0){
 			scanf("%s",input);
 			chdir(input);
+		} else if(strcmp(input,"set")==0){
+			sety();
 		} else {
 			printf("%s\n", "Falsch Eingabe");
 		}
+		
 	}
 }
 
 int forky() {
-	pid_t pid = fork();
+	pid = fork();
 	if (pid == 0) {
 		//Child
-		printf("BubuBaba\n");
+		printf("Jetzt brauchen wir deinen Programmnamen:\n");
 		programmStarty();
+		
 	} else {
 		//Parent
-		waitpid(0,NULL,WNOHANG);
-		printf("Daddy\n");
+		waitpid(pid,0,0);
 	}
 }
 
 int programmStarty() {
-    char input[256];
-	scanf("%s",input);
-	printf("%s\n", input);
+    char input[256]="";
+    char path[256];
+    char biny[256] = "/bin/";
+    char useybiny[256] ="/usr/bin/";
+    char zwischy[256] = "";
+    //Bei firefox macht er es richtig, sonst total falsch
+	do {
+		scanf("%s",input);
+		strcat(zwischy, input);
+		printf("%s", zwischy);
+	} while (!strcmp(input,"")); 
+
+
+	//realpath(input, path);
+	strcat(useybiny,zwischy);
+	//möglicherweise klappt die if Bedingung, printf will es uns aber nicht sagen
+	if (access(useybiny, F_OK) != -1) {
+		printf("ja");
+		//Die Parameterliste müssen wir noch einmal prüfen
+		execlp(useybiny,zwischy, NULL);
+	} else {
+		printf("nein");
+		strcat(biny,zwischy);
+		
+		//Die Parameterliste müssen wir noch einmal prüfen
+		execvp(biny, zwischy);
+	}
+}
+
+//Hier fehlt quasi alles
+int sety(){
+	printf("Hier werden irgendwann Umgebungsvariablen gesetzt\n");
 }
