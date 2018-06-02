0f99ad435ba6c2be8053bafdd7c2b5a63bbe11c7
André <akirsch1@fh-bielefeld.de>
2018-06-02 12:12:58 +0200

    Artikelseite füllen

--- Webbasierte Anwendungen/WBA_Website/public_html/js/article.js
+++ Webbasierte Anwendungen/WBA_Website/public_html/js/article.js
@@ -35,3 +35,20 @@ class Task extends Article {
         this.type = "task";
     }
 }
+
+let articleConverter = new ArticleConverter();
+
+function showArticle(id) {
+    article = articleConverter.jsonToArticle(localStorage.getItem(id));
+    document.getElementById("articleBody").class += article.type;
+    document.getElementById("articleTitel").innerHTML = article.title;
+    document.getElementById("articleTitel").style = "color: " + article.color + ";";
+    document.getElementById("articleContent").innerHTML = article.text;
+    document.getElementById("articleStart").innerHTML = "Start: " + article.start;
+    document.getElementById("articleEnd").innerHTML = "Ende: " + article.end;    
+}
+
+window.onload = function() {
+    let urlParams = new URLSearchParams(window.location.search);
+    showArticle(urlParams.get("id"));
+};
