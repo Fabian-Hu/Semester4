d340cb682a82fec7eb994e16880b9f098cf836bd
Tim Meier <meiertim745@gmail.com>
2018-06-09 14:42:33 +0200

    Teamviewer ist putt, oder mein Laptop

--- EmbeddedSystem/Abgabe6/fibonacci/fibonacci.ino
+++ EmbeddedSystem/Abgabe6/fibonacci/fibonacci.ino
@@ -13,11 +13,23 @@ void setup() {
   // init first two Fibonacci numbers
   fibData[0] = 1;
   fibData[1] = 1;
-
+ //"MOVS r7, %[index]\n\t"
   asm volatile(
-
-  // TODO: insert program here
-  
+  "MOV r4, %[fib] \n\t"
+  "loopfibo:\n\t"
+  "LDR r5, [r4, #1]\n\t"
+  "LDR r6, [r4, #0]\n\t"
+  "ADD r6, r5\n\t"
+  "STR r6, [r4, #2]\n\t"
+  "ADD r4, #1\n\t"
+  "LDR r6, [r4, #0]\n\t"
+  "ADD r7, #1\n\t"
+  "CMP r7, %[index]\n\t"
+  "IT ne\n\t"
+  "BNE loopfibo"
+  :
+  :[fib] "r" (fibData), [index] "r" (lastFiboIndex)
+  :"r4","r5","r6","r7","cc","memory"
   );
 
 }
