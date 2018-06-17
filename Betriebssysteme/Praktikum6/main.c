e661a77a691b904b010f10d434fef94842ab0b94
TSMeier <meiertim745@gmail.com>
2018-06-17 21:17:20 +0200

    betriebssysteme

--- Betriebssysteme/Praktikum6/main.c
+++ Betriebssysteme/Praktikum6/main.c
@@ -11,6 +11,7 @@ typedef struct
 	Queue queue;
 	pthread_cond_t *cond;
 	int sync;
+	int done;
 } Daten;
 Daten daten;
 
@@ -24,7 +25,7 @@ int add(char* file,Daten*daten){
 	return 0;
 }
 char* get_head(Daten*daten){
-	char* value = (char*)queue_head(daten->queue);
+	char* value = (char*) daten->queue->head->value;
 	queue_delete(daten->queue);
 	return value;
 }
@@ -32,10 +33,7 @@ char* get_head(Daten*daten){
 void * lock(Daten* daten){	
 	pthread_mutex_lock(daten->mutex);
 	int temp = daten->sync;
-	printf("%i",temp);
-	printf("lock\n");
 	while(temp){
-		printf("while\n");
 		pthread_cond_wait (daten->cond, daten->mutex);
 	}
 	daten->sync = 1;
@@ -44,13 +42,12 @@ void * lock(Daten* daten){
 void *unlock(void* args){
 	Daten* daten = (Daten*) args;
 	daten->sync = 0;
-	printf("unlock\n");
 	pthread_mutex_unlock(daten->mutex);
 	pthread_cond_broadcast(daten->cond);
 }
 
 void *get_files(void *args){	
-	printf("Dateien werden gelesen.\n");
+	printf("Dateien werden gelesen %s.\n",dir);
 	Daten* daten = (Daten*) args;
 	struct dirent *de; // Pointer auf den aktuellen Ort
     DIR *dr = opendir(dir); // Der Ordner wird geöffnet
@@ -58,18 +55,44 @@ void *get_files(void *args){
         printf("Der Ordner kann nicht geoeffnet werden.\n" );
         return (void*)NULL;
     }
+    long length;
+    char * buffer = 0;
+    char result[256];
+    FILE * file;
     while ((de = readdir(dr)) != NULL){ // es wird solange gelesen, wie es dateien gibt.
 		
         if(!strstr(de->d_name,".compr") // .compr wird ignoriert
         && !strcmp(de->d_name,"..")==0 // .. wird ignoriert
-        && !strcmp(de->d_name,".")==0){ // . wird ignoriert
+        && !strcmp(de->d_name,".")==0 // 8 == Datei, 4 == Ordner
+        && de->d_type == 8){ // . wird ignoriert
 			lock(daten);
-			//printf("%i\n",de->d_type); // 8 == Datei, 4 == Ordner
-            printf("%s\n", de->d_name);
-            add(de->d_name,daten);
+            printf("Neu: %s\n", de->d_name);
+            memset(result, 0, sizeof(result));
+			strcpy(result,dir);
+            //strcat(result,dir);
+            //strcat(result,de->d_name);
+            //printf("%s\n",result);
+            strcat(result,de->d_name);
+			// printf(result);
+            file = fopen(result,"r");
+            //printf("test");
+            fseek(file,0,SEEK_END);
+            length = ftell(file);
+            //printf("test");
+            buffer = (char*)malloc(sizeof(char)*length);
+            if(buffer){
+				fread(buffer,sizeof(char)*length,1,file);
+				printf("Datei gelesen: %s\n",buffer);
+			}
+	
+			
+			fclose(file);
+ ;          add(buffer,daten);
             unlock(daten);
 		}
 	}
+	printf("\nDateien fertig gelesen\n");
+	daten->done = 1;
     closedir(dr);    
 	return (void*)NULL;
 }
@@ -77,25 +100,26 @@ void *get_files(void *args){
 
 void *compress(void* args){
 	Daten * daten = (Daten*) args;
-	printf("Neuer Prozess in Compress\n");
-	while(queue_empty(daten->queue));
-	printf("Daten sind da.\n");
+	printf("Neuer Prozess\n");
+	while(queue_empty(daten->queue) && !daten->done);
 	while(!queue_empty(daten->queue)){ // kein null wert
-		printf("in der WHile");
 		lock(daten);
-		printf("Komprimiert: %s\n",get_head(daten));
+		char * string = get_head(daten);
+		printf("Komprimiert: %s\n",string);
+		//Result *result = compress_string(string);
 		// <=hier muss komprimiert werden
-		
 		// wenn fertig
 		unlock(daten);
 	}
 }
 
-void create_threads(void*args){ // anzahl threads variable
+void create_threads(void*args,int count){ // anzahl threads variable
 	pthread_t reader;
 	pthread_t comp;
 	pthread_t comp2;
 	pthread_t comp3;
+	pthread_t *test[count];
+	
 	int x =0;
 	int status1 = 0;
 	int status2 = 0;
@@ -110,17 +134,25 @@ void create_threads(void*args){ // anzahl threads variable
 	pthread_cond_init(daten->cond,NULL);
 	// der reader
 	status1 = pthread_create(&reader, NULL, get_files, args);
+	
+	for(int i =0;i<count;i++){
+		test[i] = (pthread_t*) malloc(sizeof(pthread_t));
+		pthread_create(test[i], NULL, compress, args);
+	}
 	//ein compressor
-	status2 = pthread_create(&comp, NULL, compress, args);
+	/*status2 = pthread_create(&comp, NULL, compress, args);
 	status3 = pthread_create(&comp2, NULL, compress, args);
-	status4 = pthread_create(&comp3, NULL, compress, args);
+	status4 = pthread_create(&comp3, NULL, compress, args);*/
 	
 	
-	printf("Es geht weiter\n");
 	pthread_join(reader,NULL);
-	pthread_join(comp,NULL);
+	/*pthread_join(comp,NULL);
 	pthread_join(comp2,NULL);
-	pthread_join(comp3,NULL);
+	pthread_join(comp3,NULL);*/
+	
+	for(int i = 0;i<count;i++){
+		pthread_join(*test[i],NULL);
+	}
 		
 }
 
@@ -129,7 +161,8 @@ void main(int argc,char ** argv){
 	strcpy(dir,argv[1]);
 	data->queue = queue_create();
 	data->sync = 0;
-	create_threads(data);
+	data->done = 0;
+	create_threads(data,1);
 	queue_destroy(data->queue);// am ende alles freigeben
 	pthread_mutex_destroy(data->mutex);
 }
